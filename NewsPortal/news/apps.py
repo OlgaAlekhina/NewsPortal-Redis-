0a9ab2fb122ec6fcfa8b4a86fb0b6465d43a4ceb
OlgaAlekhina <89858498+OlgaAlekhina@users.noreply.github.com>
2022-02-14 19:04:23 +0300

    Update apps.py

--- NewsPortal/news/apps.py
+++ NewsPortal/news/apps.py
@@ -8,12 +8,6 @@ class NewsConfig(AppConfig):
     def ready(self):
         import news.signals
 
-red = redis.Redis(
-    host='redis-16707.c54.ap-northeast-1-2.ec2.cloud.redislabs.com',
-    port='16707',
-    password='49yFCATjG4vXzZD0IcWDM6CcvoOEahOk'
-)
-
 
 
 
